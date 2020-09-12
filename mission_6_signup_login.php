0a1dc89a820a489d1759b066bb35a9575f041911
AKIRA-MIKI <69907764+AKIRA-MIKI@users.noreply.github.com>
2020-09-13 05:38:58 +0900

    Create mission_6_signup_login.php

---
+++ mission_6_signup_login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <title>Login</title>
+ </head>
+ <body>
+   <h1>ログイン</h1>
+   <form  action="login.php" method="post">
+     <input type="email" name="mail" placeholder = "メールアドレス">
+     <input type="password" name="password" placeholder = "パスワード">
+     <button type="submit" name="submit">ログイン</button>
+   </form>
+   <h1>新規登録</h1>
+   <form action="mission_6_signup.php" method="post">
+     <input type="email" name="mail" placeholder = "メールアドレス">
+     <input type="password" name="password" placeholder = "パスワード">
+     <button type="submit" name="submit">新規登録</button>
+   </form>
+ </body>
+</html>
